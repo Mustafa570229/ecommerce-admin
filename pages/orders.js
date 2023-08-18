9932fb19bf274a148121e1be28ba48e5c66e2104
Your Name <mustafaalabohasne@gmail.com>
2023-08-18 05:47:44 +0300

    ecommerce admin

---
+++ pages/orders.js
@@ -0,0 +1,9 @@
+import Layout from "@/components/Layout"
+
+const orders = () => {
+  return (
+    <Layout>orders</Layout>
+  )
+}
+
+export default orders
---
+++ pages/settings.js
@@ -0,0 +1,9 @@
+import Layout from "@/components/Layout"
+
+const setting = () => {
+  return (
+    <Layout>setting</Layout>
+  )
+}
+
+export default setting
